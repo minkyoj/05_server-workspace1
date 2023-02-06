ba8f211abef725dfb624c6a7e6ea219b3568f7fe
kyoding <minkyo0816@gmail.com>
2023-02-06 11:21:36 +0900

    0206

--- 2_JSP/src/com/kh/controller/PizzaServlet.java
+++ 2_JSP/src/com/kh/controller/PizzaServlet.java
@@ -1,6 +1,8 @@
 package com.kh.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -39,18 +41,75 @@ public class PizzaServlet extends HttpServlet {
 		
 		request.setCharacterEncoding("UTF-8");
 		
-		String userName = request.getParameter("userName");
+		String name = request.getParameter("userName");
 		String phone = request.getParameter("phone");
 		String address = request.getParameter("address");
 		String message = request.getParameter("message");
-		String[] pizza = request.getParameterValues("pizza");
-		String[] topping = request.getParameterValues("topping");
-		String[] side = request.getParameterValues("side");
+		String pizza = request.getParameter("pizza");
+		String[] toppings = request.getParameterValues("topping");
+		String[] sides = request.getParameterValues("side");
 		String payment = request.getParameter("payment");
+		
+		// 3) 요청처리 (db에 sql문 실행하러 > Service > Dao)
+		
+		int price = 0; // 합계금액을 저장할 변수
+		switch(pizza) {
+		case "콤비네이션피자" : price += 5000; break;
+		case "치즈피자" : price += 6000; break;
+		case "포테이토피자" :
+		case "고구마피자" : price += 7000; break;
+		case "불고기피자" : price += 8000; break;
+		}
+		
+		if(toppings != null) { // 토핑이 null이 아닐때만 돌리기
+			for(int i=0; i<toppings.length; i++) {
+				switch(toppings[i]) {
+				case "고구마무스" :
+				case "콘크림무스" : price += 1500; break;
+				case "파인애플토핑" :
+				case "치즈토핑" : price += 2000; break;
+				case "치즈파이트" :
+				case "치즈크러스트" : price += 3000; break;
+				}
+		}
+		}
+		
+		if(sides != null) { // 사이드가 null이 아닐때만 돌리기
+			for(int i=0; i<sides.length; i++) {
+				switch(sides[i]) {
+				case "콜라" :
+				case "사이다" : price += 2000; break;
+				case "갈릭소스" :
+				case "핫소스" : price += 300; break;
+				case "피클" : 
+				case "파마산치즈가루" : price += 500; break;
+				}
+			}
+		}
+		
+		// 4) 요청 처리 후 사용자가 보게 될 응답페이지 (결제페이지) 만들기
+		//	  응답페이지(jsp)를 선택해서 포워딩
+		//	  단, 응답페이지에서 필요한 데이터가 있다면 담아서 포워딩 할 것!
+		//	  > request의 attribute 영역에 담기
+		
+		request.setAttribute("name", name);
+		request.setAttribute("phone", phone);
+		request.setAttribute("address", address);
+		request.setAttribute("message", message);
+		request.setAttribute("pizza", pizza);
+		request.setAttribute("toppings", toppings);
+		request.setAttribute("sides", sides);
+		request.setAttribute("payment", payment);
+		request.setAttribute("price", price);
+		
+		// 응답할 뷰 (jsp) 선택
+		RequestDispatcher view = request.getRequestDispatcher("views/pizza/pizzaPayment.jsp");
+		// 선택된 뷰가 사용자에게 보여지도록 포워딩
+		view.forward(request, response);
 		
 		phone = phone.substring(0, 3) + "-" + phone.substring(3, 7) + "-" + phone.substring(7, 11);
 		
-		System.out.println("userName : " + userName);
+		System.out.println("userName : " + name);
 		System.out.println("phone : " + phone);
 		System.out.println("address : " + address);
 		System.out.println("message : " + message);
@@ -61,16 +120,16 @@ public class PizzaServlet extends HttpServlet {
 			System.out.println("pizza : " + String.join("-", pizza));
 		}
 		
-		if(topping == null) {
+		if(toppings == null) {
 			System.out.println("topping : 없음");
 		}else {
-			System.out.println("topping : " + String.join("-", topping));
+			System.out.println("topping : " + String.join("-", toppings));
 		}
 		
-		if(side == null) {
+		if(sides == null) {
 			System.out.println("side : 없음");
 		}else {
-			System.out.println("side : " + String.join("-", side));
+			System.out.println("side : " + String.join("-", sides));
 		}
 		
 		System.out.println("payment : " + payment);
